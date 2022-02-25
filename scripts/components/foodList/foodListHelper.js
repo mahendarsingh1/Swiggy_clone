42bac9027f4482440d6f432eca789781a05cdd1c
mahendarsingh1 <mahendarsinghrathod111@gmail.com>
2022-02-26 00:32:58 +0530

    cleaned view helper

--- scripts/components/foodList/foodListHelper.js
+++ scripts/components/foodList/foodListHelper.js
@@ -1,33 +1,26 @@
-function paragraph(text,classList){ 
-    return `<p class="${arrayToString(classList)}">${text}</p>`
-}
-
-function button(text,classList){
-    return `<button class="${arrayToString(classList)}">${text}</button>`
-}
-
-function img(src, classList){
-    return `<img class="${arrayToString(classList)}" src=${src}>`
-}
-
 function flexDiv(item){
-    return ('<div class="flex-space-between">' +
-                '<div>' +
-                    `<h3 class="margin-tb-8">${item.heading}</h3>` +
-                    paragraph(item.amount, ["margi-tb-8"]) +
-                '</div>' +
-                button('ADD',["food-list-item-button"]) +
-            '</div>');
+    return `
+            <div class="flex-space-between">
+                <div>
+                    <h3 class="margin-tb-8">${item.heading}</h3>
+                    <p class="margin-tb-8">${item.amount}</p>
+                </div>
+                <button class="food-list-item-button">ADD</button>
+            </div>
+        `;
 }
 
 function foodItem(item){
-    return '<div class="food-list-item">' +
-                img(item.img, ["margin-tb-8", "height-20"]) +
-                flexDiv(item) +
-                paragraph(item.description, ["margin-tb-8", "op-6"]) +
-            '</div>' + 
-            '<hr></hr>';
+    return `
+        <div class="food-list-item">
+            <img src=${item.img} class="margin-tb-8 height-20">
+                ${flexDiv(item)}
+                <p class="margin-tb-8 op-6">${item.description}</p>
+            </div>
+            <hr></hr>
+        `;
 }
+
 
 export function foodList(data){
     let list_innerhtml = '';
@@ -44,13 +37,4 @@ export function foodList(data){
         list_innerhtml = list_innerhtml + section_innerhtml;
     }
     return list_innerhtml;
-}
-
-
-function arrayToString(arr){
-    let str='';
-    arr.forEach(ele => {
-        str = str + String(ele)+' ';
-    });
-    return str;
 }
